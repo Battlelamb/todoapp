41fd02317e599a930ccb106e8b9fc0234d565cc2
Ömer EREN <erenomer@gmail.com>
2021-07-07 19:01:22 +0300

    delete işlemi eklendi

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for
 from flask.globals import request
 from wtforms import Form, StringField, validators
 from flask_mongoengine import MongoEngine
-
+from uuid import uuid4
 
 app = Flask(__name__)
 
@@ -18,12 +18,16 @@ db.init_app(app)
 
 
 class User(db.Document):
+    _id = db.StringField()
+    uuid = db.StringField()
     firstname = db.StringField()
     lastname = db.StringField()
     adres = db.StringField()
 
     def to_json(self):
         return {
+            "_id": self._id,
+            "uuid": self.uuid,
             "firstname": self.firstname,
             "lastname": self.lastname,
             "adres": self.adres
@@ -67,24 +71,52 @@ def register():
     form = RegisterForm(request.form)
 
     if request.method == "POST":  # Sunucuya Gönderme İşlemi
+
         firstname = form.firstname.data  # data == text == değeri
+        uuid = uuid4().hex
         lastname = form.lastname.data
         adres = form.adres.data
 
         # print(form.firstname.data)
         # print(form.lastname.data)
 
-        user = User(firstname=firstname, lastname=lastname, adres=adres)
+        user = User(firstname=firstname, lastname=lastname,
+                    adres=adres, uuid=uuid)
         user.save()
 
         return redirect(url_for("register"))
     else:
         users = list(User.objects)
-        for x in users:
-            print(x.firstname)
+        # for x in users:
+        #     print(x.firstname)
         return render_template("register.html", form=form, users=users)
 
-        
+
+@app.route("/delete/<id>", methods=["GET"])
+def delete_user(id):
+    try:
+        print(User)
+        print("ID: ", id)
+        # user = User.objects(uuid=str(id)).first()
+        user = User.objects(uuid=str(id)).delete()
+        # user.delete()
+        print(user)
+        # users = list(User.objects)
+        # for x in users:
+        #     print(x.firstname)
+        # try:
+        #     user.delete()
+        # except Exception as ex:
+        #     print("****", ex, "****")
+        if not user:
+            print("yok artık")
+        else:
+            user.delete()
+    except Exception as ex:
+        print("((((", ex, "))))")
+
+    return redirect(url_for("register"))
+
 # Register Page End
 
 # @app.route("/layout") # => http://127.0.0.1:5000/layout
